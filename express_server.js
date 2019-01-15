ddeda35e62c3bfe30b0d878e9a2db9f85d829df8
Julia <juliaromanowski@gmail.com>
2019-01-15 21:50:11 +0000

    connecting insert url page with get/post

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -22,6 +25,16 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // debug statement to see POST parameters
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
+
 app.get("/urls/:id", (req, res) => {
     let templateVars = {
         shortURL: req.params.id,
@@ -29,6 +42,9 @@ app.get("/urls/:id", (req, res) => {
     };
     res.render("urls_show", templateVars);
 });
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
